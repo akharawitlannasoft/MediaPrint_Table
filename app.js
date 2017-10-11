48d4b940278d4f3d0397fa00e9602b6c7950135a
akharawitlannasoft <31607299+akharawitlannasoft@users.noreply.github.com>
2017-10-11 14:12:08 +0700

    Finish the first project
    -ใบสั่งผสม
    -Punching Process Check Sheet
    -Sintering Process Check Sheet #1
    -Sintering Process Check Sheet #2
    -Sintering Process Check Sheet #3
    -Inspection Process Check Sheet #1
    -Inspection Process Check Sheet #2
    -Inspection Process Check Sheet #3

---
+++ app.js
@@ -0,0 +1,7 @@
+var msg = 'Hello World';
+var msg = 'Hello World' + 1;
+var msg = 'Hello World' + 2;
+var msg = 'Hello World' + 3;
+var msg = 'Hello World' + 4;
+console.log(msg)
+sdfassssdfdffasddfsfadfasdfsdsdfasdfsfsdff
